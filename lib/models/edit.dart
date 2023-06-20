df025d53bf4a7a1a33983ba02568dcb447fcb516
FirnandoSaay <firnandosaay483@gmail.com>
2023-06-20 12:37:20 +0700

    Commit Firnando
    Menyelsaikan edit.dart dari liyak08

--- lib/models/edit.dart
+++ lib/models/edit.dart
@@ -124,4 +124,97 @@ Widget build(BuildContext context) {
       )
     ],
   ),
-  
+  body: Form(
+    key: _formKey,
+    child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Title',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 5),
+              TextFormField(
+                controller: title,
+                decoration: InputDecoration(
+                    hintText: "Type Note Title",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Note Title is Required!';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Content',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 5),
+              TextFormField(
+                controller: content,
+                keyboardType: TextInputType.multiline,
+                minLines: 5,
+                maxLines: null,
+                decoration: InputDecoration(
+                    hintText: 'Type Note Content',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Note Content id Required!';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: const Text(
+                  "submit",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  
+                  if (_formKey.currentState!.validate()) {
+                    _onUpdate(context);
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
